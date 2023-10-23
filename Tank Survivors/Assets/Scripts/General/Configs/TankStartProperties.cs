2420a2f0308dd4fd2238b130380ddd50ce02b35b
morz <morz21st@gmail.com>
2023-10-23 23:15:58 +0300

    Update TankStartProperties

--- Tank Survivors/Assets/Scripts/General/Configs/TankStartProperties.cs
+++ Tank Survivors/Assets/Scripts/General/Configs/TankStartProperties.cs
@@ -1,4 +1,5 @@
 ﻿using Tank;
+using DataStructs;
 using UnityEngine;
 
 namespace General.Configs
@@ -14,9 +15,59 @@ namespace General.Configs
         private float health;
         public float Health => health;
 
+        [SerializeField]
+        private uint experience;
+        public uint Experience => experience;
+
+        [SerializeField]
+        private float speed;
+        public float Speed => speed;
+
+        [SerializeField]
+        private float pickupRadius;
+        public float PickupRadius => pickupRadius;
+
+        [SerializeField]
+        private float armor;
+        public float Armor => armor;
+
+        [SerializeField]
+        private Percentage criticalChance;
+        public Percentage CriticalChance => criticalChance;
+
+        [SerializeField]
+        private Percentage evadeChance;
+        public Percentage EvadeChance => evadeChance;
+
+        [SerializeField]
+        private Percentage damageModifier;
+        public Percentage DamageModifier => damageModifier;
+
+        [SerializeField]
+        private Percentage projectileSize;
+        public Percentage ProjectileSize => projectileSize;
+
+        [SerializeField]
+        private Percentage rangeModifier;
+        public Percentage RangeModifier => rangeModifier;
+
+        [SerializeField]
+        private Percentage fireRateModifier;
+        public Percentage FireRateModifier => fireRateModifier;
+
         public void AssignStartProperties(TankImpl tank)
         {
             tank.Health.BaseValue = health;
+            tank.Experience.ValueContainer.BaseValue = experience;
+            tank.Speed.SourceValue = speed;
+            tank.PickupRadius.SourceValue = pickupRadius;
+            tank.Armor.SourceValue = armor;
+            tank.CriticalChance.SourceValue = criticalChance;
+            tank.EvadeChance.SourceValue = evadeChance;
+            tank.DamageModifier.SourceValue = damageModifier;
+            tank.ProjectileSize.SourceValue = projectileSize;
+            tank.RangeModifier.SourceValue = rangeModifier;
+            tank.FireRateModifier.SourceValue = fireRateModifier;
         }
     }
 }
