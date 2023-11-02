f5d69fa52ed22caf867ea208420414d69c4dfc02
morz <morz21st@gmail.com>
2023-11-02 18:50:34 +0300

    Add sheet scrolling

---
+++ Tank Survivors/Assets/Scripts/Sheet/SheetScrolling.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Sheet
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    [AddComponentMenu("Sheet.SheetScrolling")]
+    public class SheetScrolling : MonoBehaviour
+    {
+        private Material material;
+
+        private void Awake()
+        {
+            material = GetComponent<MeshRenderer>().material;
+        }
+
+        private void Update()
+        {
+            Vector2 newOffset = new Vector2(
+                transform.position.x / transform.localScale.x,
+                transform.position.y / transform.localScale.y);
+
+            material.mainTextureOffset = newOffset;
+        }
+    }
+}
