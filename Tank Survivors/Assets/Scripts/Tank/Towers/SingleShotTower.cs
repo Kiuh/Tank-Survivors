305b400bc0b411133e7dd8e737e8c6f6f8f5c813
morz <morz21st@gmail.com>
2023-11-20 19:06:47 +0300

    Add tower

---
+++ Tank Survivors/Assets/Scripts/Tank/Towers/SingleShotTower.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Tank.Towers
+{
+    public class SingleShotTower : MonoBehaviour, ITower, ICanRotate
+    {
+        [SerializeField]
+        private Transform shotPoint;
+
+        public Vector3 GetShotPoint()
+        {
+            return shotPoint.position;
+        }
+
+        public void RotateTo(Vector2 direction)
+        {
+            transform.rotation = Quaternion.Euler(
+                0f,
+                0f,
+                Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f
+            );
+        }
+    }
+}
