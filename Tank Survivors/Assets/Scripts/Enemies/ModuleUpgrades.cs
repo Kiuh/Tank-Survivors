1662d92e6cfaaf96d490bf6539bbed040f3bd9b8
ShovelD <gimnazist.ds@gmail.com>
2024-03-02 19:23:45 +0300

    added more modules upgrades

--- Tank Survivors/Assets/Scripts/Enemies/ModuleUpgrades.cs
+++ Tank Survivors/Assets/Scripts/Enemies/ModuleUpgrades.cs
@@ -36,4 +36,76 @@ namespace Enemies
             );
         }
     }
+
+    [Serializable]
+    [LabelText("Health")]
+    public class HealthUpgrade : IModuleUpgrade
+    {
+        public void ApplyUpgrade(EnemyProducer producer)
+        {
+            HealthModule module = producer.Producer.Modules.GetConcrete<HealthModule, IModule>();
+            float value = producer.ProgressorProperties.Value;
+            if (producer.ProgressorProperties.Mode == ProgressorMode.fromCurrent)
+            {
+                value *= module.Health.GetModifiedValue();
+            }
+            else
+            {
+                value *= module.Health.SourceValue;
+            }
+            module.Health.Modifications.Add(
+                new(MathOperation.Plus.ToFunction(value), ModificationPriority.Medium)
+            );
+        }
+    }
+
+    [Serializable]
+    [LabelText("Cooldown")]
+    public class CooldownUpgrade : IModuleUpgrade
+    {
+        public void ApplyUpgrade(EnemyProducer producer)
+        {
+            AttackCooldownModule module = producer.Producer.Modules.GetConcrete<
+                AttackCooldownModule,
+                IModule
+            >();
+            float value = producer.ProgressorProperties.Value;
+            if (producer.ProgressorProperties.Mode == ProgressorMode.fromCurrent)
+            {
+                value *= module.Cooldown.GetModifiedValue();
+            }
+            else
+            {
+                value *= module.Cooldown.SourceValue;
+            }
+            module.Cooldown.Modifications.Add(
+                new(MathOperation.Minus.ToFunction(value), ModificationPriority.Medium)
+            );
+        }
+    }
+
+    [Serializable]
+    [LabelText("XP")]
+    public class ExpeirienceUpgrade : IModuleUpgrade
+    {
+        public void ApplyUpgrade(EnemyProducer producer)
+        {
+            ExperienceModule module = producer.Producer.Modules.GetConcrete<
+                ExperienceModule,
+                IModule
+            >();
+            float value = producer.ProgressorProperties.Value;
+            if (producer.ProgressorProperties.Mode == ProgressorMode.fromCurrent)
+            {
+                value *= module.DropAmount.GetModifiedValue();
+            }
+            else
+            {
+                value *= module.DropAmount.SourceValue;
+            }
+            module.DropAmount.Modifications.Add(
+                new(MathOperation.Plus.ToFunction(value), ModificationPriority.Medium)
+            );
+        }
+    }
 }
