982f892887187779ee0b695c571c606ceaeafd41
LeshaTola <tola.lesha@mail.ru>
2024-03-28 16:06:38 +0300

    Remove null checking

--- Tank Survivors/Assets/Scripts/Tank/Weapons/ModulesUpgrades/CannonModulesUpgrades.cs
+++ Tank Survivors/Assets/Scripts/Tank/Weapons/ModulesUpgrades/CannonModulesUpgrades.cs
@@ -24,9 +24,6 @@ namespace Tank.Weapons.ModulesUpgrades.Cannon
 
         private IEnumerable GetAllPositions()
         {
-            if (CannonPosition == null)
-                return null;
-
             return CannonPositioner.Properties.Select(x => x.Name);
         }
     }
