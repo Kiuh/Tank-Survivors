040a15e5637d1355371fa9f65e80632ca62a5321
ShovelD <gimnazist.ds@gmail.com>
2024-04-02 20:26:56 +0300

    Added ability to follow to hp bar

---
+++ Tank Survivors/Assets/Scripts/Panels/PlayerFollower.cs
@@ -0,0 +1,43 @@
+﻿using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using Tank;
+using UnityEngine;
+
+namespace Panels
+{
+    public class PlayerFollower : SerializedMonoBehaviour
+    {
+        public enum Offset
+        {
+            scene,
+            value
+        }
+
+        [OdinSerialize]
+        private RectTransform canvas;
+
+        [OdinSerialize]
+        private TankImpl tank;
+
+        [OdinSerialize]
+        [EnumToggleButtons]
+        private Offset offsetType = Offset.scene;
+
+        [OdinSerialize]
+        private Vector3 offset;
+
+        private void Start()
+        {
+            if (offsetType == Offset.scene)
+            {
+                offset = canvas.position - tank.transform.position;
+            }
+        }
+
+        private void Update()
+        {
+            Vector3 position = tank.transform.position;
+            canvas.position = position + offset;
+        }
+    }
+}
