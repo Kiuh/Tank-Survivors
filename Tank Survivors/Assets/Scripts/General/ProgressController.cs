04d6f26e4a388fa05e0785ba14bb79d80392a958
Aleksey Tola <tola.lesha@mail.ru>
2024-04-03 21:28:13 +0300

    Move Panels Prefabs Add Win Panel Add Win

---
+++ Tank Survivors/Assets/Scripts/General/ProgressController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace General
+{
+    public class ProgressController : MonoBehaviour
+    {
+        [SerializeField]
+        EnemyGenerator enemyGenerator;
+
+        [SerializeField]
+        [ReadOnly]
+        public int BossCount { get; } = 1;
+
+        public int Progress { get; private set; } = 0;
+
+        public event Action OnWin;
+
+        private void Start()
+        {
+            enemyGenerator.OnBossDead += OnBossDead;
+        }
+
+        private void OnDestroy()
+        {
+            enemyGenerator.OnBossDead -= OnBossDead;
+        }
+
+        private void OnBossDead()
+        {
+            Progress++;
+            if (Progress == BossCount)
+            {
+                OnWin?.Invoke();
+            }
+        }
+    }
+}
---
+++ Tank Survivors/Assets/Scripts/Panels/Win/Controller.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using General;
+using UnityEngine;
+
+namespace Panels.Win
+{
+    [AddComponentMenu("Panels.Win.Controller")]
+    public class Controller : MonoBehaviour
+    {
+        [SerializeField]
+        private ProgressController progressController;
+
+        [SerializeField]
+        private General.Timer timer;
+
+        [SerializeField]
+        private View view;
+
+        private void Awake()
+        {
+            progressController.OnWin += Show;
+        }
+
+        private void Show()
+        {
+            Time.timeScale = 0.0f;
+            view.ShowWinPanel(GetInfoString(), progressController.Progress);
+        }
+
+        public void Hide()
+        {
+            Time.timeScale = 1.0f;
+            view.HideLosePanel();
+        }
+
+        public void LeaveGame()
+        {
+            Time.timeScale = 1.0f;
+            ScenesController.Instance.LoadScene(InGameScene.MainScene);
+        }
+
+        private string GetInfoString()
+        {
+            StringBuilder stringBuilder = new();
+            _ = stringBuilder.AppendLine($"Time: {timer.CurrentTime:0.0}");
+            return stringBuilder.ToString();
+        }
+    }
+}
