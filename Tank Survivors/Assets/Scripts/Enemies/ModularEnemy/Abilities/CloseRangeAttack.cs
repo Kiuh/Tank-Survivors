2bec1b10ec61bc05ff952344731a046b62d3c033
ShovelD <gimnazist.ds@gmail.com>
2024-04-07 15:12:41 +0300

    Added Close range attack ability

---
+++ Tank Survivors/Assets/Scripts/Enemies/ModularEnemy/Abilities/CloseRangeAttack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Common;
+using Enemies.Bosses.Abilities;
+using Sirenix.OdinInspector;
+using Tank;
+using Unity.Loading;
+using UnityEngine;
+
+namespace Enemies.Abilities
+{
+    [Serializable]
+    [LabelText("CloseRangeAttack")]
+    public class CloseRangeAttack : IAbility
+    {
+        private DamageModule damage;
+        private AttackCooldownModule attackCooldown;
+        private Collider2D collider;
+        private TankImpl tank;
+        private Collider2D tankCollider;
+        public bool IsActive { get; set; }
+        private float timer = 0f;
+
+        public List<IModule> GetModules()
+        {
+            return new() { new AttackCooldownModule() };
+        }
+
+        public void Initialize(Enemy enemy, TankImpl tank, List<IModule> modules)
+        {
+            this.tank = tank;
+            collider = enemy.Collider;
+            tankCollider = tank.GetComponent<BoxCollider2D>();
+            damage = modules.GetConcrete<DamageModule, IModule>();
+            attackCooldown = modules.GetConcrete<AttackCooldownModule, IModule>();
+            enemy.FixedUpdatableAbilities.Add(this);
+            IsActive = true;
+        }
+
+        public void Use()
+        {
+            timer -= Time.deltaTime;
+            if (collider.IsTouching(tankCollider))
+            {
+                if (timer <= 0)
+                {
+                    tank.TakeDamage(damage.Damage.GetModifiedValue());
+                    timer = attackCooldown.Cooldown.GetModifiedValue();
+                    return;
+                }
+            }
+        }
+    }
+}
