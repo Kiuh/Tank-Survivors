ce6190b05f47e506b32cfe835a100a920f86edeb
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-06-27 00:15:29 +0300

    Add background scrolling optimization

---
+++ Tank Survivors/Assets/Scripts/General/ParallaxBackground.cs
@@ -0,0 +1,35 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace General
+{
+    [AddComponentMenu("Scripts/General/General.ParallaxBackground")]
+    internal class ParallaxBackground : MonoBehaviour
+    {
+        [Required]
+        [SerializeField]
+        private MeshRenderer meshRenderer;
+
+        [Required]
+        [SerializeField]
+        private Transform targetTransform;
+
+        [SerializeField]
+        private float scrollSpeed = 1f;
+
+        [SerializeField]
+        private Vector3 startPosition;
+
+        private void Start()
+        {
+            startPosition = transform.position;
+        }
+
+        private void Update()
+        {
+            transform.rotation = Quaternion.Euler(Vector3.zero);
+            Vector2 offset = targetTransform.position - startPosition;
+            meshRenderer.material.mainTextureOffset = offset * scrollSpeed;
+        }
+    }
+}
